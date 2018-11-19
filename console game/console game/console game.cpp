cdfcbaed5e1b12db51e19593ac1fb4a919fe4edb
daer5 <44932250+daer5@users.noreply.github.com>
2018-11-18 21:03:49 -0500

    controls

--- console game/console game/console game.cpp
+++ console game/console game/console game.cpp
@@ -47,31 +47,32 @@ char dotDotDot[3][4] = {
 	"                                                      "
 };
 */
-int x = 1;
+int x = 8;
 int y = 1;
-bool control_array[5];
-char gameMap[20][41] = {
-	"########################################",
-	"#                                      #",
-	"#                                      #",
-	"#                                      #",
-	"#                                      #",
-	"#                                      #",
-	"#                                      #",
-	"#                                      #",
-	"#                                      #",
-	"#                                      #",
-	"#                                      #",
-	"#                                      #",
-	"#                                      #",
-	"#                                      #",
-	"#                                      #",
-	"#                                      #",
-	"#                                      #",
-	"#                                      #",
-	"#                                      #",
-	"########################################",
+bool control_array[6];
+char gameMap[20][106] = {
+	"#########################################################################################################",
+	"#      |-                                                                                               #",
+	"#      |                                                                                                #",
+	"#      |                                                                                                #",
+	"#      |                                                                                                #",
+	"#      |                                                                                                #",
+	"#                                                                                                       #",
+	"#                                                                                                       #",
+	"#                                                                                                       #",
+	"#                                                                                                       #",
+	"#                                                                                                       #",
+	"#                                                                                                       #",
+	"#                                                                                                       #",
+	"#                                                                                                       #",
+	"#                                                                                                       #",
+	"#                                                                                                       #",
+	"#                                                                                                       #",
+	"#                                                                                                       #",
+	"#                                                                                                       #",
+	"#########################################################################################################",
 };
+
 /*
 void waitForUserInput(std::string textStart, std::string textMiddle,std::string textEnd = "continue") {
 	bool waiting = true;
@@ -102,58 +103,102 @@ void renderGame() {
 	SetConsoleTextAttribute(hConsole, 11);
 	while (inGame) {
 		system("cls");																
-		for (int printGameMap = 0; printGameMap < 20; printGameMap++) {				//prints mapp array each frame.
-			std::cout << gameMap[printGameMap] << std::endl;
-		}
+		
 
 		userInput();																//check for controls - will change array value accordingly
 		gameLogic();																//check for array value for key input - will draw character accordingly
+		for (int printGameMap = 0; printGameMap < 20; printGameMap++) {				//prints mapp array each frame.
+			std::cout << gameMap[printGameMap] << std::endl;
+		}
 		system("pause>nul");														//pause and wait for everything - you're moma accordingly 
 	}
 }
 
 void gameLogic() {
 	if (control_array[2] == true) {                     //if user presses up
-		int y2 = y - 1;							
+		int y2 = y - 1;
 		if (gameMap[y2][x] == ' ') {			//if the square above is blank,
 			gameMap[y][x] = ' ';				//make the current square blank,
 			y--;								//change original y value to the value up one
-			gameMap[y][x] = '-';				//and make the new value occupied by -
-			
+			gameMap[y][x] = '^';				//and make the new value occupied by -
+
 			std::cout << "up" << std::endl;
 		}
+		else {
+			std::cout << "border" << std::endl;
+		}
+	}
 	if (control_array[3] == true) {						//if user presses down
 		int y2 = y + 1;
 		if (gameMap[y2][x] == ' ') {
 			gameMap[y][x] = ' ';
 			y++;
-			gameMap[y][x] = '-';
-			
+			gameMap[y][x] = 'v';
+
 			std::cout << "down" << std::endl;
 		}
+		else {
+			std::cout << "border" << std::endl;
+		}
 	}
+	if (control_array[0]) {
+		int x2 = x + 1;
+		if (gameMap[y][x2] == ' ') {
+			gameMap[y][x] = ' ';
+			x++;
+			gameMap[y][x] = '>';
+
+			std::cout << "right" << std::endl;	
+		}
+		else {
+			std::cout << "border" << std::endl;
+		}
+
+	}
+	if (control_array[1]) {
+		int x2 = x - 1;
+		if (gameMap[y][x2] == ' ') {
+			gameMap[y][x] = ' ';
+			x--;
+			gameMap[y][x2] = '<';
+
+			std::cout << "left" << std::endl;
+		}
+		else {
+			std::cout << "border" << std::endl;
+		}
+
+	}
+	if (control_array[5]) {
+		int y1 = y - 1;
+		int y2 = y - 2;
+		if (gameMap[y1][x] && gameMap[y2][x] == ' ') {
+
+		}
+		std::cout << "jump" << std::endl;
 	}
 }
-
 void userInput() {
-	for (int makeKeyStateFalse = 0; makeKeyStateFalse < 5; makeKeyStateFalse++) {
+	for (int makeKeyStateFalse = 0; makeKeyStateFalse < 6; makeKeyStateFalse++) {
 		control_array[makeKeyStateFalse] = false;
 	}
 
-	if (GetAsyncKeyState(0x57) != 0) {			//w UP
+	if (GetAsyncKeyState('W') != 0) {			//w UP
 
 		control_array[2] = true;
+	
 	}
-	if (GetAsyncKeyState(0x53) != 0) {			//s DOWN
-
+	if (GetAsyncKeyState('S') != 0) {			//s DOWN
+	
 		control_array[3] = true;
+	
 	}
-	if (GetAsyncKeyState(0x44) != 0) {			//d RIGHT
+	if (GetAsyncKeyState('D') != 0) {			//d RIGHT
 
 		control_array[0] = true;
 
 	}
-	if (GetAsyncKeyState(0x41) != 0) {			//a LEFT
+	if (GetAsyncKeyState('A') != 0) {			//a LEFT
 
 		control_array[1] = true;
 
@@ -161,22 +206,32 @@ void userInput() {
 	if (GetAsyncKeyState(VK_UP) != 0) {			//up
 
 		control_array[2] = true;
+	
 	}
 	if (GetAsyncKeyState(VK_DOWN) != 0) {		//down
 
 		control_array[3] = true;
+	
 	}
 	if (GetAsyncKeyState(VK_LEFT) != 0) {		//left
 
 		control_array[1] = true;
+	
 	}
 	if (GetAsyncKeyState(VK_RIGHT) != 0) {		//right
 
 		control_array[0] = true;
+	
 	}
 	if (GetAsyncKeyState(VK_RETURN) != 0) {		//enter
 
 		control_array[4] = true;
+	
+	}
+	if (GetAsyncKeyState(VK_SPACE) != 0) {
+		
+		control_array[5] = true;
+	
 	}
 	//system("pause>nul");
 }
